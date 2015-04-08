321f5bbf88da48b3bb67aec061da147568f6ad04
Michael Yu <ItzWarty@gmail.com>
2015-04-08 15:59:11 -0700

    Update Copyright in AssemblyInfo.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("The Dargon Project")]
 [assembly: AssemblyProduct("Dargon.Audits.Impl.Db")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © The Dargon Project 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
